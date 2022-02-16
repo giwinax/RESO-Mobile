f01820916171bac544816c7c29ca0b208d69c8c1
Stanislav <giwinax@gmail.com>
2022-02-16 17:30:49 +0300

    Initial Commit

---
+++ RESO Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RESO Mobile
+//
+//  Created by s b on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
